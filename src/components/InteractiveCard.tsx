3019a613633fb7e0ac4c8fe55841c8d1ed184d4f
FreezingH2O <airadajitauleeruk@gmail.com>
2025-03-13 13:28:44 +0700

    edit interactiveCard

--- src/components/InteractiveCard.tsx
+++ src/components/InteractiveCard.tsx
@@ -1,19 +1,20 @@
 'use client'
 import React from 'react';
 
-export default function Card({children,contentName}: 
+export default function InteractiveCard({children,contentName}: 
     {children: React.ReactNode, contentName:string}){
 
     const onCardMouse= (event: React.SyntheticEvent)=>{
-        if(event.type=='mouseover'){
+        if(event.type==='mouseover'){
             event.currentTarget.classList.remove('shadow-lg')
             event.currentTarget.classList.add('shadow-2xl')
             event.currentTarget.classList.add('bg-neutral-200')
+
             
         }else{
             event.currentTarget.classList.remove('shadow-2xl')
-            event.currentTarget.classList.remove('bg-neutral-200')
             event.currentTarget.classList.add('shadow-lg')
+            event.currentTarget.classList.remove('bg-neutral-200')
            
         }
     }
@@ -24,7 +25,7 @@ export default function Card({children,contentName}:
 
     return (
         <div
-        className="rounded-lg bg-white w-1/5 shadow-lg transition-all duration-200"
+        className="rounded-lg bg-white w-1/5 shadow-lg "
         onClick={select} 
         onMouseOver={onCardMouse} 
         onMouseOut={onCardMouse}>
